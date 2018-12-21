641b42f52dc6f755b6d9a0488e7c8d63a8d9c876
LeoKnox <LeoKnox@users.noreply.github.com>
2018-12-20 23:58:03 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_socketio import SocketIO
+from mysqlconnection import connectToMySQL
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'make it so number one'
@@ -7,6 +9,16 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def sessions():
+    mysql = connectToMySQL('pw2')
+    player = mysql.query_db('SELECT * FROM pw2.Char WHERE idChar=1')
+    session.name = 'Christmas Orc'
+    print(player.Name)
+    session['atk'] = 7
+    session['def'] = 10
+    session['agi'] = 6
+    session['mag'] = 4
+    session['luk'] = 4
+    session['hp'] = 58
     return render_template('session.html')
 
 def messageReceived(methods=['GET', 'POST']):
@@ -14,6 +26,7 @@ def messageReceived(methods=['GET', 'POST']):
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
+    json['def']=7
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
 
